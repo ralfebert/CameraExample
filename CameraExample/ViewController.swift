402ea8e8aac64f42866e5e48e73ca94f955e0708
Ralf Ebert <info@ralfebert.de>
2018-11-27 16:59:19 +0100

    camera example

---
+++ CameraExample/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  CameraExample
+//
+//  Created by Ralf Ebert on 27.11.18.
+//  Copyright © 2018 Ralf Ebert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func takePhoto(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+
+        // print out the image size as a test
+        print(image.size)
+    }
+
+}
+
